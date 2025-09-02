1b3dd993ec7883b83765c85dc7c9b59c57e7b363
Marcelo <marceloflopal@gmail.com>
2025-09-02 11:57:38 -0500

    Subiendo ejercicio 1

---
+++ Semana 3 - FA/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_3___FA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1 ()
+        {
+            string carrera, nombre;
+            Console.Write("Ingrese su nombre ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese su carrera ");
+            carrera = Console.ReadLine();
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de fundamentos de algoritmos de la carrera {carrera}");
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+            int num1, num2;
+            Console.Write("Ingrese el primer numero entero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero entero");
+            num2 = int.Parse(Console.ReadLine());
+            
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ Semana3_Python/Semana3_Python.py
@@ -0,0 +1,7 @@
+﻿def ejr1p():
+    nombre = input(("Ingrese su nombre: "))
+    carrera = input(("Ingrese su carrera: "))
+    print(f"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}")
+  
+
+ejr1p()
